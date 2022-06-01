783cd23b597d3b7b99668da97763e89c84d5be8e
Phi Van Ngoc <ngocketit@gmail.com>
2022-06-01 15:14:59 +0300

    Make some changes

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Github Actions
+          Learn CICD & Github Actions
         </a>
       </header>
     </div>
